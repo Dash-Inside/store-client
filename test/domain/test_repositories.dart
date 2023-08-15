c86c616ccb468326aa09b7f22adf29292c29c4fe
pocket-red-fox <pocket.red.fox@gmail.com>
2023-08-16 01:14:28 +0300

    chore: ignore unnecessary rules

--- test/domain/test_repositories.dart
+++ test/domain/test_repositories.dart
@@ -1,3 +1,5 @@
+//ignore_for_file: unused_import
+
 import 'package:mockito/annotations.dart';
 import 'package:store_client/src/domain/repository/library_repository.dart';
 import 'package:store_client/src/domain/repository/messenger_repository.dart';
