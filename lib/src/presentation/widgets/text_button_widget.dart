f5d5c7d066efc6475cbc1442847f362f4dd50f78
pocket-red-fox <pocket.red.fox@gmail.com>
2023-08-16 00:10:27 +0300

    feat: add base widgets & pages

---
+++ lib/src/presentation/widgets/text_button_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextButtonWidget extends StatelessWidget {
+  const TextButtonWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
