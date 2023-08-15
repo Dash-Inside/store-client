b0b458a6aa2d51b0b88bc62b2901afaabfa4185a
pocket-red-fox <pocket.red.fox@gmail.com>
2023-08-16 01:01:45 +0300

    feat: add test files

---
+++ test/domain/test_repositories.dart
@@ -0,0 +1,9 @@
+import 'package:mockito/annotations.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+import 'package:store_client/src/domain/repository/messenger_repository.dart';
+import 'package:store_client/src/domain/repository/user_data_repository.dart';
+
+@GenerateNiceMocks([MockSpec<LibraryRepository>()])
+@GenerateNiceMocks([MockSpec<MessengerRepository>()])
+@GenerateNiceMocks([MockSpec<UserDataRepository>()])
+import 'test_repositories.mocks.dart';
