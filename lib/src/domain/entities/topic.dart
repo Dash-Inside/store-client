d695b6e3740d98ffcb814c1858d7ec5cee211319
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-16 14:07:15 +0300

    feat: add topic entity

--- lib/src/domain/entities/topic.dart
+++ lib/src/domain/entities/topic.dart
@@ -1,1 +1,13 @@
-class Topic {}
+class Topic {
+  final int id;
+  final String title;
+  final String data;
+  final List<String> links;
+
+  Topic(
+    this.id,
+    this.title,
+    this.data,
+    this.links,
+  );
+}
