d93d381f2e073ea380328ef662766b9a5078a2d1
diffygg <np.01@inbox.ru>
2023-08-16 17:59:41 +0300

    feat: add LogInUseCase and add LogInUseCaseParams

--- lib/src/domain/usecases/user_data/log_in_usecase.dart
+++ lib/src/domain/usecases/user_data/log_in_usecase.dart
@@ -1,1 +1,31 @@
-class LogInUseCase {}
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/core/usecases/usecases.dart';
+import 'package:store_client/src/data/repositories/remote/user_data_server_repository.dart';
+import 'package:store_client/src/domain/entities/user_data.dart';
+import 'package:store_client/src/domain/repository/user_data_repository.dart';
+
+class LogInUseCase extends UseCase<UserData, LogInUseCaseParams> {
+  final UserDataRepository userDataRepository = services.get<UserDataServerRepository>();
+
+  @override
+  FutureOr<Either<Failure, UserData>> call(LogInUseCaseParams params) {
+    return userDataRepository.loginUser(
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class LogInUseCaseParams {
+  final String email;
+  final String password;
+
+  LogInUseCaseParams(
+    this.email,
+    this.password,
+  );
+}
