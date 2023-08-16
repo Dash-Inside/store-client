d0c0568c01f6d7561b6ca1f7b88b5edcf97f1f23
FastetStream <okurenkov.denis112@gmail.com>
2023-08-16 22:59:52 +0300

    refactor: change code style

--- lib/src/presentation/pages/log_in_page.dart
+++ lib/src/presentation/pages/log_in_page.dart
@@ -1,29 +1,27 @@
 import 'package:flutter/material.dart';
-
-import '../widgets/filled_button_widget.dart';
-import '../widgets/text_button_widget.dart';
-import '../widgets/text_field_widget.dart';
+import 'package:store_client/src/presentation/widgets/filled_button_widget.dart';
+import 'package:store_client/src/presentation/widgets/text_button_widget.dart';
+import 'package:store_client/src/presentation/widgets/text_field_widget.dart';
 
 class LoginPage extends StatelessWidget {
   static const flex = 3;
-  static const double sizedboxheight = 8.0;
-  static const double letterspacing = 0.5;
+  static const double sizedBoxHeight = 8.0;
+  static const double letterSpacing = 0.5;
 
-  static const double edgeinsetsall = 16.0;
-  static const double fontsize2 = 16.0;
-  static const double fontsize1 = 28.0;
+  static const double edgeInsetsAll = 16.0;
+  static const double fontSizeThin = 16.0;
+  static const double fontSizeHeaders = 28.0;
   LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     ColorScheme colorScheme = Theme.of(context).colorScheme;
-
     Color colorSurf = colorScheme.surface;
 
     return Scaffold(
       backgroundColor: colorSurf,
       body: Container(
-        margin: EdgeInsets.all(edgeinsetsall),
+        margin: EdgeInsets.all(edgeInsetsAll),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
@@ -31,15 +29,15 @@ class LoginPage extends StatelessWidget {
             Text(
               'Dash-Inside',
               style: TextStyle(
-                fontSize: fontsize1,
+                fontSize: fontSizeHeaders,
                 fontWeight: FontWeight.w500,
-                letterSpacing: letterspacing,
+                letterSpacing: letterSpacing,
               ),
             ),
             Text(
               'Non-Profit Organization',
               style: TextStyle(
-                fontSize: fontsize2,
+                fontSize: fontSizeThin,
                 fontWeight: FontWeight.w300,
               ),
             ),
@@ -47,12 +45,12 @@ class LoginPage extends StatelessWidget {
               flex: flex,
             ),
             TextFieldWidget(text: 'Email'),
-            SizedBox(height: sizedboxheight),
+            SizedBox(height: sizedBoxHeight),
             TextFieldWidget(text: 'Password'),
             Spacer(),
             TextButtonWidget(text: 'Restore code'),
             SizedBox(
-              height: sizedboxheight,
+              height: sizedBoxHeight,
             ),
             FilledButtonWidget(hintText: 'Log in'),
           ],
