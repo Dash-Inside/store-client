f101b9aaa794b07fff3200b1cd3d392833c8d0c5
diffygg <np.01@inbox.ru>
2023-08-16 16:51:57 +0300

    refactor: improve code quality

--- lib/src/domain/repository/user_data_repository.dart
+++ lib/src/domain/repository/user_data_repository.dart
@@ -3,16 +3,16 @@ import 'package:store_client/core/failure/failure.dart';
 import 'package:store_client/src/domain/entities/user_data.dart';
 
 abstract class UserDataRepository {
-  /// [changeUserNameById] returns Either [Failure] if there is some errors or [Unit] if there is no errors.
-  /// [changeUserNameById] takes [userData], [newUserName] that used for change userName by id.
-  Future<Either<Failure, Unit>> changeUserNameById({
+  /// [changeUserNameById] returns Either [Failure] if there is some errors or [UserData] if there is no errors.
+  /// [changeUserNameById] takes [userData], [newUserName] that used for change userName by [userData] and get [UserData].
+  Future<Either<Failure, UserData>> changeUserNameById({
     required UserData userData,
     required String newUserName,
   });
 
-  /// [changeAvatarUrlById] returns Either [Failure] if there is some errors or [Unit] if there is no errors.
-  /// [changeAvatarUrlById] takes [userData], [newAvatarUrl] that used for change avatarUrl by id.
-  Future<Either<Failure, Unit>> changeAvatarUrl({
+  /// [changeAvatarUrlById] returns Either [Failure] if there is some errors or [UserData] if there is no errors.
+  /// [changeAvatarUrlById] takes [userData], [newAvatarUrl] that used for change avatarUrl by [userData] and get [UserData].
+  Future<Either<Failure, UserData>> changeAvatarUrl({
     required UserData userData,
     required String newAvatarUrl,
   });
