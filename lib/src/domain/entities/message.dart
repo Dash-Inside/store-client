df805b0beacf962bde5a7b1efd325b21549947b1
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-16 14:42:15 +0300

    fix: add equatable and reuired params to massage

--- lib/src/domain/entities/message.dart
+++ lib/src/domain/entities/message.dart
@@ -1,11 +1,14 @@
-class Message {
+import 'package:equatable/equatable.dart';
+
+class Message extends Equatable {
   final int id;
   final String data;
   final int senderId;
-
-  Message(
-    this.id,
-    this.data,
-    this.senderId,
-  );
+  @override
+  List<Object?> get props => [id];
+  Message({
+    required this.id,
+    required this.data,
+    required this.senderId,
+  });
 }
