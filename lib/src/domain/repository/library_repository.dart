d50c9d7405b4e9bc7b01f7f60b1c82ac62d67d31
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-16 15:58:56 +0300

    fix: change return type at getAllSection and getAllTopic

--- lib/src/domain/repository/library_repository.dart
+++ lib/src/domain/repository/library_repository.dart
@@ -1,29 +1,30 @@
 import 'package:dartz/dartz.dart';
+import 'package:store_client/src/domain/entities/section.dart';
 import 'package:store_client/src/domain/entities/topic.dart';
 
 import '../../../core/failure/failure.dart';
 
 abstract class LibraryRepository {
-  /// [addFavouriteTopic] returns Either [Failure] if there is some errors or [Topic] if there is no errors
-  /// [read] takes [id] to save at favourite [Topic]
-  Future<Either<Failure, Topic>> addFavouriteTopic({required int id});
+  /// [addFavoriteTopic] returns Either [Failure] if there is some errors or [Topic] if there is no errors
+  /// [addFavoriteTopic] takes [id] to save at favourite [Topic]
+  Future<Either<Failure, Unit>> addFavoriteTopic({required int id});
+
+  /// [removeFavoriteTopic] returns Either [Failure] if there is some errors or [Topic] if there is no errors
+  /// [removeFavoriteTopic] takes [id] remove favourite [Topic]
+  Future<Either<Failure, Unit>> removeFavoriteTopic({required int id});
 
   /// Takes [title],[data],[links] to add new [Topic]
-  Future<Topic> addTopic({required String title, required String data, required List<String> links});
+  Future<Either<Failure, Unit>> addTopic({required String title, required String data, required List<String> links});
 
-  /// Show all [sections]
-  Future<void> getAllSections();
+  /// Show all [Section]
+  Future<List<Section>> getAllSections();
 
-  /// Show all [topic]
-  Future<void> getAllTopic();
+  /// Show all [Topic]
+  Future<List<Topic>> getAllTopic();
 
-  /// [getTopicByID] returns Either [Failure] if there is some errors or [Topic] if there is no errors
-  /// [getTopicByID] takes [id] to return the [Topic]
-  Future<Either<Failure, Topic>> getTopicByID({required int id});
-
-  /// [removeFavouriteTopic] returns Either [Failure] if there is some errors or [Topic] if there is no errors
-  /// [removeFavouriteTopic] takes [id] remove favourite [Topic]
-  Future<Either<Failure, Unit>> removeFavouriteTopic({required int id});
+  /// [getTopicDataByID] returns Either [Failure] if there is some errors or [Topic] if there is no errors
+  /// [getTopicDataByID] takes [id] to return the [Topic]
+  Future<Either<Failure, Topic>> getTopicDataByID({required int id});
 
   /// [searchTopicByTitle] returns Either [Failure] if there is some errors or [Topic] if there is no errors
   /// [searchTopicByTitle] takes [title] to return the [Topic]
