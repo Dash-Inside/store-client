92b8144c3598e46305572bd20148a7c03bc1e720
FastetStream <okurenkov.denis112@gmail.com>
2023-08-16 15:16:07 +0300

    feat: add text field widget

--- lib/src/presentation/widgets/text_field_widget.dart
+++ lib/src/presentation/widgets/text_field_widget.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
 
 class TextFieldWidget extends StatelessWidget {
-  const TextFieldWidget({super.key});
+  final String text;
+  TextFieldWidget({
+    super.key,
+    required this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double contentPadding = 8.0;
+    double cirRad = 8.0;
+    Color colorPr = Theme.of(context).colorScheme.primary;
+    OutlineInputBorder outlineBord = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(cirRad),
+      borderSide: BorderSide(
+        color: colorPr,
+        width: 1.0,
+      ),
+    );
+    OutlineInputBorder outlineBord1 = OutlineInputBorder(
+      borderRadius: BorderRadius.circular(cirRad),
+      borderSide: BorderSide(
+        color: colorPr,
+        width: 1.0,
+      ),
+    );
+
+    return TextField(
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(contentPadding),
+        isDense: true,
+        focusedBorder: outlineBord,
+        enabledBorder: outlineBord1,
+        hintText: text,
+      ),
+    );
   }
 }
