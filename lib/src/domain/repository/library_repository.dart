8b6138c741c7a9a4e2c3c4a3873d244431f03c13
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-16 15:35:51 +0300

    feat: create library_repository interface

--- lib/src/domain/repository/library_repository.dart
+++ lib/src/domain/repository/library_repository.dart
@@ -1,1 +1,31 @@
-abstract class LibraryRepository {}
+import 'package:dartz/dartz.dart';
+import 'package:store_client/src/domain/entities/topic.dart';
+
+import '../../../core/failure/failure.dart';
+
+abstract class LibraryRepository {
+  /// [addFavouriteTopic] returns Either [Failure] if there is some errors or [Topic] if there is no errors
+  /// [read] takes [id] to save at favourite [Topic]
+  Future<Either<Failure, Topic>> addFavouriteTopic({required int id});
+
+  /// Takes [title],[data],[links] to add new [Topic]
+  Future<Topic> addTopic({required String title, required String data, required List<String> links});
+
+  /// Show all [sections]
+  Future<void> getAllSections();
+
+  /// Show all [topic]
+  Future<void> getAllTopic();
+
+  /// [getTopicByID] returns Either [Failure] if there is some errors or [Topic] if there is no errors
+  /// [getTopicByID] takes [id] to return the [Topic]
+  Future<Either<Failure, Topic>> getTopicByID({required int id});
+
+  /// [removeFavouriteTopic] returns Either [Failure] if there is some errors or [Topic] if there is no errors
+  /// [removeFavouriteTopic] takes [id] remove favourite [Topic]
+  Future<Either<Failure, Unit>> removeFavouriteTopic({required int id});
+
+  /// [searchTopicByTitle] returns Either [Failure] if there is some errors or [Topic] if there is no errors
+  /// [searchTopicByTitle] takes [title] to return the [Topic]
+  Future<Either<Failure, Topic>> searchTopicByTitle({required String title});
+}
