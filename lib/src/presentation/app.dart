8f33c6698de6f6b196fee03314159bed8cbfea5a
ShySpyDie <92712724+ShySpyDie@users.noreply.github.com>
2023-08-16 14:33:17 +0300

    feat: add material app

--- lib/src/presentation/app.dart
+++ lib/src/presentation/app.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:store_client/src/presentation/pages/log_in_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: Color.fromRGBO(255, 97, 47, 1),
+          surface: Colors.white,
+          secondary: Colors.black,
+          tertiary: Color.fromRGBO(0, 0, 0, 0),
+        ),
+      ),
+      home: LogInPage(),
+    );
   }
 }
