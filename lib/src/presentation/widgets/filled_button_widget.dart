1bc2315f343d15f20700139c9b2f032a15f6aa89
FastetStream <okurenkov.denis112@gmail.com>
2023-08-16 15:10:39 +0300

    feat: add field button widget

--- lib/src/presentation/widgets/filled_button_widget.dart
+++ lib/src/presentation/widgets/filled_button_widget.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
 class FilledButtonWidget extends StatelessWidget {
-  const FilledButtonWidget({super.key});
+  final String text;
+  const FilledButtonWidget({
+    super.key,
+    required this.text,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    double fontsize = 20.0;
+    double circleborder = 8.0;
+
+    return SizedBox(
+      width: double.infinity,
+      child: FilledButton(
+        style: ButtonStyle(
+          shape: MaterialStatePropertyAll(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(circleborder),
+              ),
+            ),
+          ),
+        ),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: TextStyle(
+            height: 1.0,
+            fontSize: fontsize,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
+    );
   }
 }
