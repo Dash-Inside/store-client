447ef2d30c5075d305b3bd655a533379515f89a4
pocket-red-fox <pocket.red.fox@gmail.com>
2023-08-16 13:26:49 +0300

    fix: update entity

--- lib/src/domain/entities/section.dart
+++ lib/src/domain/entities/section.dart
@@ -1,8 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 class Section {
-  String id;
+  final int id;
+  String title;
+  int? topicId;
 
   Section({
     required this.id,
+    required this.title,
+    this.topicId,
   });
 }
