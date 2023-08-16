a7c9c27875d21d3fbf8788a158945a78031fa143
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-16 14:52:21 +0300

    fix: codestyle

--- lib/src/domain/entities/message.dart
+++ lib/src/domain/entities/message.dart
@@ -4,8 +4,10 @@ class Message extends Equatable {
   final int id;
   final String data;
   final int senderId;
+
   @override
   List<Object?> get props => [id];
+
   Message({
     required this.id,
     required this.data,
