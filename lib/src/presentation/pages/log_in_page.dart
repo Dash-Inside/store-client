63e835b829bdd190ed76b7c79344e46b0ddd745b
FastetStream <okurenkov.denis112@gmail.com>
2023-08-16 22:43:43 +0300

    feat: add log in page

--- lib/src/presentation/pages/log_in_page.dart
+++ lib/src/presentation/pages/log_in_page.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
-class LogInPage extends StatelessWidget {
-  const LogInPage({super.key});
+import '../widgets/filled_button_widget.dart';
+import '../widgets/text_button_widget.dart';
+import '../widgets/text_field_widget.dart';
+
+class LoginPage extends StatelessWidget {
+  static const flex = 3;
+  static const double sizedboxheight = 8.0;
+  static const double letterspacing = 0.5;
+
+  static const double edgeinsetsall = 16.0;
+  static const double fontsize2 = 16.0;
+  static const double fontsize1 = 28.0;
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    Color colorSurf = colorScheme.surface;
+
+    return Scaffold(
+      backgroundColor: colorSurf,
+      body: Container(
+        margin: EdgeInsets.all(edgeinsetsall),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            Text(
+              'Dash-Inside',
+              style: TextStyle(
+                fontSize: fontsize1,
+                fontWeight: FontWeight.w500,
+                letterSpacing: letterspacing,
+              ),
+            ),
+            Text(
+              'Non-Profit Organization',
+              style: TextStyle(
+                fontSize: fontsize2,
+                fontWeight: FontWeight.w300,
+              ),
+            ),
+            Spacer(
+              flex: flex,
+            ),
+            TextFieldWidget(text: 'Email'),
+            SizedBox(height: sizedboxheight),
+            TextFieldWidget(text: 'Password'),
+            Spacer(),
+            TextButtonWidget(text: 'Restore code'),
+            SizedBox(
+              height: sizedboxheight,
+            ),
+            FilledButtonWidget(hintText: 'Log in'),
+          ],
+        ),
+      ),
+    );
   }
 }
