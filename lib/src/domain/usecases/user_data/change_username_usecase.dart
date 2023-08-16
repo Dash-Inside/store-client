ac54eb92cce26906573670467d02b111d0cfba85
diffygg <np.01@inbox.ru>
2023-08-16 17:43:06 +0300

    feat: add ChangeUserNameUseCase and UseCaseParams

--- lib/src/domain/usecases/user_data/change_username_usecase.dart
+++ lib/src/domain/usecases/user_data/change_username_usecase.dart
@@ -1,1 +1,31 @@
-class ChangeUsernameUseCase {}
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/core/usecases/usecases.dart';
+import 'package:store_client/src/data/repositories/remote/user_data_server_repository.dart';
+import 'package:store_client/src/domain/entities/user_data.dart';
+import 'package:store_client/src/domain/repository/user_data_repository.dart';
+
+class ChangeUserNameUseCase extends UseCase<UserData, ChangeUserNameUseCaseParams> {
+  final UserDataRepository userDataRepository = services.get<UserDataServerRepository>();
+
+  @override
+  FutureOr<Either<Failure, UserData>> call(ChangeUserNameUseCaseParams params) {
+    return userDataRepository.changeUserNameUserData(
+      userData: params.userData,
+      newUserName: params.newUserName,
+    );
+  }
+}
+
+class ChangeUserNameUseCaseParams {
+  final UserData userData;
+  final String newUserName;
+
+  ChangeUserNameUseCaseParams(
+    this.userData,
+    this.newUserName,
+  );
+}
