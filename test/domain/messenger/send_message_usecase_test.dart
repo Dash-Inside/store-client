c1b4d688c0debeb5b3ab61fc563874c64eb4feff
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-17 14:06:55 +0300

    feat: add test SendMessUseCase for Repository

--- test/domain/messenger/send_message_usecase_test.dart
+++ test/domain/messenger/send_message_usecase_test.dart
@@ -1,1 +1,35 @@
-void main() {}
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:store_client/src/data/repositories/remote/messenger_server_repository.dart';
+import 'package:store_client/src/domain/entities/message.dart';
+import 'package:store_client/src/domain/usecases/messenger/send_message_usecase.dart';
+
+@GenerateNiceMocks([MockSpec<MessengerServerRepository>()])
+import 'send_message_usecase_test.mocks.dart';
+
+void main() {
+  final Message message = Message(id: 2, data: "data", senderId: 4);
+  test(
+    "sender_message_usecase_test",
+    () async {
+      // Act.
+      final MessengerServerRepository messengerServerRepository = MockMessengerServerRepository();
+      when(messengerServerRepository.sendMessage(message: message)).thenAnswer(
+        (realInvocation) async {
+          return Right(unit);
+        },
+      );
+
+      // Arrange.
+      final SendMessageUsecase sendMessageUsecase = SendMessageUsecase();
+      final result = await sendMessageUsecase.call(message);
+
+      // Assert.
+      verify(messengerServerRepository.sendMessage(message: message)).called(1);
+      verifyNoMoreInteractions(messengerServerRepository);
+      expect(result, Right(unit));
+    },
+  );
+}
