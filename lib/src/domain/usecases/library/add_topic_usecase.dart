4638c26c68a7ab6deffcce1eafd9d764222811a2
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-17 14:32:57 +0300

    feat: add AddTopicUseCase

--- lib/src/domain/usecases/library/add_topic_usecase.dart
+++ lib/src/domain/usecases/library/add_topic_usecase.dart
@@ -1,1 +1,23 @@
-class AddTopicUseCase {}
+import 'package:dartz/dartz.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/usecases/usecases.dart';
+import 'package:store_client/src/domain/entities/topic.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+
+class AddTopicUseCase extends UseCase<Topic, Topic> {
+  final LibraryRepository libraryRepository;
+
+  AddTopicUseCase({
+    required this.libraryRepository,
+  });
+  @override
+  Future<Either<Failure, Topic>> call(Topic params) async {
+    Either<Failure, Unit> newTopic = await libraryRepository.addTopic(
+      title: params.title,
+      data: params.data,
+      links: params.links,
+    );
+
+    return right(newTopic as Topic);
+  }
+}
