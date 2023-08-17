1981deb44a3c472d92c4691c6158288bf8ed0214
Daniil <danyildyb@gmail.com>
2023-08-17 20:04:33 +0300

    feat: add test for SearchTopicUsecase

--- test/domain/library/search_topic_by_title_usecase_test.dart
+++ test/domain/library/search_topic_by_title_usecase_test.dart
@@ -1,1 +1,40 @@
-void main() {}
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/src/domain/entities/topic.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+import 'package:store_client/src/domain/usecases/library/search_topic_by_title_usecase.dart';
+
+@GenerateNiceMocks([MockSpec<LibraryRepository>()])
+import 'search_topic_by_title_usecase_test.mocks.dart';
+
+void main() {
+  const String title = 'title';
+  final Topic topic = Topic(
+    id: 1,
+    title: "title",
+    data: "data",
+    links: [],
+  );
+
+  test('search_topic_by_title_usecase_test', () async {
+    // Act.
+    final LibraryRepository libraryRepository = MockLibraryRepository();
+    when(libraryRepository.searchTopicByTitle(title: title)).thenAnswer(
+      (_) async {
+        return Right(topic);
+      },
+    );
+
+    // Arrange.
+    final SearchTopicByTitleUseCase searchTopicByTitleUseCase = SearchTopicByTitleUseCase();
+    final Either<Failure, Topic> result = await searchTopicByTitleUseCase.call(title);
+
+    // Accert.
+    verify(libraryRepository.searchTopicByTitle(title: title)).called(1);
+    verifyNoMoreInteractions(libraryRepository);
+    expect(result, Right(topic));
+  });
+}
