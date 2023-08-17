6d003ea7638a9eabdb8c5c9d5b81ddffcdb172e9
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-17 16:27:16 +0300

    feat: add GetAllSectionsUseCase

--- lib/src/domain/usecases/library/get_all_sections_usecase.dart
+++ lib/src/domain/usecases/library/get_all_sections_usecase.dart
@@ -1,1 +1,18 @@
-class GetAllSectionsUseCase {}
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/core/usecases/usecases.dart';
+import 'package:store_client/src/data/repositories/remote/library_server_repository.dart';
+import 'package:store_client/src/domain/entities/section.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+
+class GetAllSectionsUseCase extends UseCase<List<Section>, Unit> {
+  final LibraryRepository libraryRepository = services.get<LibraryServerRepository>();
+
+  @override
+  FutureOr<Either<Failure, List<Section>>> call(Unit) {
+    return libraryRepository.getAllSections();
+  }
+}
