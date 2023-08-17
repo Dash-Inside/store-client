020c6035ce1386153e780170a49a170414b6cc5b
FastetStream <okurenkov.denis112@gmail.com>
2023-08-17 19:36:22 +0300

    feat: add accaont page

--- lib/src/presentation/pages/account_page.dart
+++ lib/src/presentation/pages/account_page.dart
@@ -1,10 +1,138 @@
 import 'package:flutter/material.dart';
+import 'package:store_client/src/presentation/widgets/navigation_bar_widget.dart';
+import 'package:store_client/src/presentation/widgets/topic_widget.dart';
 
 class AccountPage extends StatelessWidget {
+  static const double editIconButtonSize = 16.0;
+  static const double widthSizedBox2 = 16.0;
+  static const double fontSizeHeaders = 28.0;
+  static const double widthSizedBox = 32.0;
+  static const double fontSizeBody = 24.0;
+  static const double fontSizeThin = 16.0;
+  static const double avatarRadius = 34.0;
+  static const double edgeInsetsMargin = 16.0;
+  static const double roleHeight = 0.8;
+
+  editOnTap() {}
+
   const AccountPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    Color colorSurface = colorScheme.surface;
+    Color colorSecondary = colorScheme.secondary;
+    Color colorPrimary = colorScheme.primary;
+
+    return Scaffold(
+      backgroundColor: colorSurface,
+      appBar: AppBar(
+        backgroundColor: colorSurface,
+        elevation: 0.0,
+        title: Text(
+          'Account',
+          style: TextStyle(
+            color: colorSecondary,
+            fontSize: fontSizeHeaders,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: EdgeInsets.all(edgeInsetsMargin),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: avatarRadius,
+                  backgroundColor: colorPrimary,
+                  child: Icon(
+                    Icons.camera_alt_outlined,
+                    color: colorSecondary,
+                  ),
+                ),
+                SizedBox(
+                  width: widthSizedBox2,
+                ),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            'Username',
+                            style: TextStyle(
+                              height: 1.0,
+                              fontSize: fontSizeBody,
+                              fontWeight: FontWeight.w400,
+                              color: colorSecondary,
+                            ),
+                          ),
+                          Spacer(),
+                          IconButton(
+                            onPressed: editOnTap,
+                            icon: Icon(
+                              Icons.mode_edit_outline_outlined,
+                              size: editIconButtonSize,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        'Role',
+                        style: TextStyle(
+                          height: roleHeight,
+                          fontSize: fontSizeThin,
+                          fontWeight: FontWeight.w300,
+                          color: colorSecondary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: widthSizedBox,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: Text(
+                  'Favorites',
+                  style: TextStyle(
+                    fontSize: fontSizeBody,
+                    fontWeight: FontWeight.w400,
+                    color: colorSecondary,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TopicWidget(text: 'Topic #1'),
+                TopicWidget(text: 'Topic #2'),
+                TopicWidget(text: 'Topic #3'),
+                TopicWidget(text: 'Topic #4'),
+                TopicWidget(text: 'Topic #5'),
+                TopicWidget(text: 'Topic #6'),
+              ],
+            ),
+          ),
+          Spacer(),
+          NavigationBarWidget(),
+        ],
+      ),
+    );
   }
 }
