f4347f6a0d817dadb326fb56870f246c609ddfdc
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-17 16:07:02 +0300

    feat: add SearchTopicByTitleUseCase

--- lib/src/domain/usecases/library/search_topic_by_title_usecase.dart
+++ lib/src/domain/usecases/library/search_topic_by_title_usecase.dart
@@ -1,1 +1,18 @@
-class SearchTopicByTitleUseCase {}
+import 'package:dartz/dartz.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/core/usecases/usecases.dart';
+import 'package:store_client/src/data/repositories/remote/library_server_repository.dart';
+import 'package:store_client/src/domain/entities/topic.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+
+class SearchTopicByTitleUseCase extends UseCase<Topic, String> {
+  final LibraryRepository libraryRepository = services.get<LibraryServerRepository>();
+
+  @override
+  Future<Either<Failure, Topic>> call(title) {
+    return libraryRepository.searchTopicByTitle(
+      title: title,
+    );
+  }
+}
