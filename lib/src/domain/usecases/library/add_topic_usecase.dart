8dee147e36cc82abf277b9a1476d7f3f49e90d2b
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-17 15:24:27 +0300

    fix: added libraryServerRepository at usecase

--- lib/src/domain/usecases/library/add_topic_usecase.dart
+++ lib/src/domain/usecases/library/add_topic_usecase.dart
@@ -4,10 +4,11 @@ import 'package:dartz/dartz.dart';
 import 'package:store_client/core/failure/failure.dart';
 import 'package:store_client/core/services/services.dart';
 import 'package:store_client/core/usecases/usecases.dart';
+import 'package:store_client/src/data/repositories/remote/library_server_repository.dart';
 import 'package:store_client/src/domain/repository/library_repository.dart';
 
 class AddTopicUseCase extends UseCase<Unit, AddTopicUseCaseParams> {
-  final LibraryRepository libraryRepository = services.get<LibraryRepository>();
+  final LibraryRepository libraryRepository = services.get<LibraryServerRepository>();
 
   @override
   FutureOr<Either<Failure, Unit>> call(AddTopicUseCaseParams params) async {
