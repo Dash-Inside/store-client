bd98e9f65a090686c50388ddeebc120d7f334e19
Daniil <danyildyb@gmail.com>
2023-08-17 13:18:15 +0300

    fix: change overrides

--- lib/src/data/repositories/remote/user_data_server_repository.dart
+++ lib/src/data/repositories/remote/user_data_server_repository.dart
@@ -5,18 +5,6 @@ import 'package:store_client/src/domain/repository/user_data_repository.dart';
 
 class UserDataServerRepository implements UserDataRepository {
   @override
-  Future<Either<Failure, UserData>> changeAvatarUrl({required UserData userData, required String newAvatarUrl}) {
-    // TODO: implement changeAvatarUrl
-    throw UnimplementedError();
-  }
-
-  @override
-  Future<Either<Failure, UserData>> changeUserNameById({required UserData userData, required String newUserName}) {
-    // TODO: implement changeUserNameById
-    throw UnimplementedError();
-  }
-
-  @override
   Future<Either<Failure, UserData>> loginUser({required String email, required String password}) {
     // TODO: implement loginUser
     throw UnimplementedError();
@@ -25,6 +13,18 @@ class UserDataServerRepository implements UserDataRepository {
   @override
   Future<Either<Failure, UserData>> restorePasswordUser({required String restoreCode, required String password, required String comfirmedPassword}) {
     // TODO: implement restorePasswordUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, UserData>> changeAvatarUrl({required UserData userData, required String newAvatarUrl}) {
+    // TODO: implement changeAvatarUrl
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, UserData>> changeUserName({required UserData userData, required String newUserName}) {
+    // TODO: implement changeUserName
     throw UnimplementedError();
   }
 }
