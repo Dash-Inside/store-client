37c35e398946bef17e741e3f91a1a9079873f99f
Daniil <danyildyb@gmail.com>
2023-08-17 19:14:05 +0300

    feat: add test for GetTopicByIdUsecase

--- test/domain/library/get_topic_by_id_usecase_test.dart
+++ test/domain/library/get_topic_by_id_usecase_test.dart
@@ -1,1 +1,18 @@
-void main() {}
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+
+@GenerateNiceMocks([MockSpec<LibraryRepository>()])
+import 'get_topic_by_id_usecase_test.mocks.dart';
+
+void main() {
+  final int id = 1;
+  test('get_topic_by_id_usecase_test', () {
+    // Act.
+    LibraryRepository libraryRepository = MockLibraryRepository();
+    when(libraryRepository.getTopicDataByID(id: id)).thenAnswer(() )
+    // Arrange.
+    // Accert.
+  });
+}
