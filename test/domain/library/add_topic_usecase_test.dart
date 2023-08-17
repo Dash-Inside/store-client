4e0b702b33b9619dbc69e5b373afd0ba0426eae4
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-17 18:33:22 +0300

    feat: add test for usecase to LibRepository

--- test/domain/library/add_topic_usecase_test.dart
+++ test/domain/library/add_topic_usecase_test.dart
@@ -1,1 +1,41 @@
-void main() {}
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+import 'package:store_client/src/domain/usecases/library/add_topic_usecase.dart';
+
+@GenerateNiceMocks([MockSpec<LibraryRepository>()])
+import 'add_topic_usecase_test.mocks.dart';
+
+void main() {
+  const String title = 'title';
+  const String data = 'data';
+  const List<String> links = ['links'];
+  final AddTopicUseCaseParams addTopicUseCaseParams = AddTopicUseCaseParams(title, data, links);
+
+  test(
+    "add_topic_usecase_test",
+    () async {
+      // Act.
+      final LibraryRepository libraryRepository = MockLibraryRepository();
+      when(libraryRepository.addTopic(title: title, data: data, links: links)).thenAnswer(
+        (_) async {
+          return Right(unit);
+        },
+      );
+
+      // Arrange.
+      final AddTopicUseCase addTopicUseCase = AddTopicUseCase();
+      final result = await addTopicUseCase.call(addTopicUseCaseParams);
+
+      // Assert.
+      verify(libraryRepository.addTopic(title: title, data: data, links: links)).called(1);
+      verifyNoMoreInteractions(libraryRepository);
+      expect(
+        result,
+        Right(unit),
+      );
+    },
+  );
+}
