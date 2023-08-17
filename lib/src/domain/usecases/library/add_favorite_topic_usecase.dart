969ff0a6598ba59dca98801bf4109fbd8e3c2998
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-17 15:46:06 +0300

    feat: add FavoriteTopicUseCase

--- lib/src/domain/usecases/library/add_favorite_topic_usecase.dart
+++ lib/src/domain/usecases/library/add_favorite_topic_usecase.dart
@@ -1,1 +1,25 @@
-class AddFavoriteTopicUseCase {}
+import 'dart:async';
+
+import 'package:dartz/dartz.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/core/usecases/usecases.dart';
+import 'package:store_client/src/data/repositories/remote/library_server_repository.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+
+class AddFavoriteTopicUseCase extends UseCase<Unit, GetFavouriteTopicID> {
+  final LibraryRepository libraryRepository = services.get<LibraryServerRepository>();
+
+  @override
+  FutureOr<Either<Failure, Unit>> call(GetFavouriteTopicID id) {
+    return libraryRepository.addFavoriteTopic(
+      id: id.id,
+    );
+  }
+}
+
+class GetFavouriteTopicID {
+  final int id;
+
+  GetFavouriteTopicID(this.id);
+}
