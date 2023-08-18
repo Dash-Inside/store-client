96b72f2cb749a45d7afac2351a8f4b9df5c37df7
diffygg <np.01@inbox.ru>
2023-08-18 16:19:38 +0300

    feat: add SharedPreferences to injector

--- lib/core/services/services.dart
+++ lib/core/services/services.dart
@@ -4,10 +4,14 @@ import 'package:get_it/get_it.dart';
 import 'package:store_client/src/data/repositories/remote/messenger_server_repository.dart';
 import 'package:store_client/src/data/repositories/remote/user_data_server_repository.dart';
 import 'package:store_client/src/data/repositories/remote/library_server_repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final GetIt services = GetIt.I;
 
-FutureOr<void> initServices() {
+FutureOr<void> initServices() async {
+  final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  services.registerLazySingleton<SharedPreferences>(() => sharedPreferences);
+
   final MessengerServerRepository messengerServerRepository = MessengerServerRepository();
   services.registerLazySingleton<MessengerServerRepository>(() => messengerServerRepository);
 
