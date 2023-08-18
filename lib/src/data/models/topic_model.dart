44124dc8eabaf349d5b1731703712dca7212ea62
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-18 13:14:52 +0300

    feat: add TopicModel

--- lib/src/data/models/topic_model.dart
+++ lib/src/data/models/topic_model.dart
@@ -1,1 +1,63 @@
-class TopicModel {}
+import 'dart:convert';
+
+import 'package:store_client/src/domain/entities/topic.dart';
+
+class TopicModel extends Topic {
+  @override
+  int get hashCode => id.hashCode ^ title.hashCode ^ data.hashCode ^ links.hashCode;
+
+  TopicModel({
+    required super.id,
+    required super.title,
+    required super.data,
+    required super.links,
+  });
+
+  factory TopicModel.fromMap(Map<String, dynamic> map) {
+    return TopicModel(
+      id: map['id'] as int,
+      title: map['attributes']['title'] as String,
+      data: map['attributes']['data'] as String,
+      links: map['attributes']['links'] as List<String>,
+    );
+  }
+
+  factory TopicModel.fromJson(String source) => TopicModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  TopicModel copyWith({
+    int? id,
+    String? title,
+    String? data,
+    List<String>? links,
+  }) {
+    return TopicModel(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      data: data ?? this.data,
+      links: links ?? this.links,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'data': data,
+      'links': links,
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  @override
+  String toString() {
+    return 'UserDataModel(id: $id, title : $title, data : $data, links: $links)';
+  }
+
+  @override
+  bool operator ==(covariant TopicModel other) {
+    if (identical(this, other)) return true;
+
+    return other.id == id && other.title == title && other.data == data && other.links == links;
+  }
+}
