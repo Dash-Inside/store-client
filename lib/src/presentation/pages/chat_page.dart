b10b1e38addcfd8d30fc71026390f76e5930bd67
danwirele <115182119+danwirele@users.noreply.github.com>
2023-08-18 14:20:44 +0300

    feat: add chat page messages view logic

--- lib/src/presentation/pages/chat_page.dart
+++ lib/src/presentation/pages/chat_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:store_client/src/domain/entities/message.dart';
 import 'package:store_client/src/presentation/widgets/incoming_message_widget.dart';
+import 'package:store_client/src/presentation/widgets/no_avatar_incoming_message_widget.dart';
 import 'package:store_client/src/presentation/widgets/outcoming_message_widget.dart';
 import 'package:store_client/src/presentation/widgets/text_field_widget.dart';
 
@@ -8,8 +10,20 @@ class ChatPage extends StatelessWidget {
   static const double fontLetterSpacing = 0.5;
   static const double iconSize = 24.0;
   static const double textFieldPadding = 8.0;
-
-  const ChatPage({super.key});
+  final ourId = 3;
+  List<Message> messageList = [
+    Message(id: 1, data: "123", senderId: 4),
+    Message(id: 2, data: "ghbgt", senderId: 4),
+    Message(id: 3, data: "ytrt", senderId: 3),
+    Message(id: 2, data: "ghbgt", senderId: 4),
+    Message(id: 3, data: "ytrt", senderId: 3),
+    Message(id: 3, data: "ytrt", senderId: 3),
+    Message(id: 3, data: "ytrt", senderId: 3),
+    Message(id: 2, data: "ghbgt", senderId: 4),
+    Message(id: 2, data: "ghbgt", senderId: 4),
+    Message(id: 2, data: "ghbgt", senderId: 4),
+  ].reversed.toList();
+  ChatPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -48,20 +62,37 @@ class ChatPage extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: ListView(
+              child: ListView.builder(
+                itemCount: messageList.length,
                 reverse: true,
-                children: [
-                  IncomingMessageWidget(text: 'Sheeeeeeeeeeeeeeeeesh'),
-                  OutcomingMessageWidget(text: 'Sheeeeeeeesh'),
-                  IncomingMessageWidget(text: 'Sheeeeeeeeeeeeeeeeesh'),
-                  OutcomingMessageWidget(text: 'Sheeeeeeeesh'),
-                  IncomingMessageWidget(text: 'Sheeeeeeeeeeeeeeeeesh'),
-                  OutcomingMessageWidget(text: 'Sheeeeeeeesh'),
-                  IncomingMessageWidget(text: 'Sheeeeeeeeeeeeeeeeesh'),
-                  OutcomingMessageWidget(text: 'Sheeeeeeeesh'),
-                  IncomingMessageWidget(text: 'Sheeeeeeeeeeeeeeeeesh'),
-                  OutcomingMessageWidget(text: 'Sheeeeeeeesh'),
-                ],
+                itemBuilder: (context, index) {
+                  if (index == 0) {
+                    if (messageList[index].senderId == ourId) {
+                      return OutcomingMessageWidget(
+                          text: messageList[index].data);
+                    } else {
+                      return IncomingMessageWidget(
+                          text: messageList[index].data);
+                    }
+                  } else {
+                    if (messageList[index].senderId !=
+                            messageList[index - 1].senderId &&
+                        messageList[index].senderId != ourId) {
+                      return IncomingMessageWidget(
+                          text: messageList[index].data);
+                    }
+                    if (messageList[index].senderId ==
+                            messageList[index - 1].senderId &&
+                        messageList[index].senderId != ourId) {
+                      return NoAvatarIncomingMessageWidget(
+                          text: messageList[index].data);
+                    }
+                    if (messageList[index].senderId == ourId) {
+                      return OutcomingMessageWidget(
+                          text: messageList[index].data);
+                    }
+                  }
+                },
               ),
             ),
             Container(
