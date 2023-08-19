e9d6436ec3f4876ecf71812b3e82e2b13acbe60d
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-19 20:28:08 +0300

    feat: add injector for UserDataServerDatasource

--- test/injector/services.dart
+++ test/injector/services.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:get_it/get_it.dart';
+import 'package:store_client/src/data/datasources/user_data_server_datasource.dart';
 import 'package:store_client/src/domain/repository/user_data_repository.dart';
 import 'package:store_client/src/domain/repository/library_repository.dart';
 import 'package:store_client/src/domain/repository/messenger_repository.dart';
@@ -12,11 +13,13 @@ final GetIt testServices = GetIt.I;
 FutureOr<void> initTestServices() {
   final UserDataRepository userDataRepository = MockUserDataRepository();
   testServices.registerLazySingleton<UserDataRepository>(() => userDataRepository);
-  
+
   final LibraryRepository libraryRepository = MockLibraryRepository();
-  testServices
-      .registerLazySingleton<LibraryRepository>(() => libraryRepository);
-  
+  testServices.registerLazySingleton<LibraryRepository>(() => libraryRepository);
+
   final MessengerRepository messengerRepository = MockMessengerRepository();
   testServices.registerLazySingleton<MessengerRepository>(() => messengerRepository);
+
+  final UserDataServerDatasource userDataServerDatasource = UserDataServerDatasource();
+  testServices.registerLazySingleton<UserDataServerDatasource>(() => userDataServerDatasource);
 }
