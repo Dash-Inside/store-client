fd2ef2dd1f2d99281e91c06aeb991fa6f79c9c93
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-19 20:27:28 +0300

    feat: update test for UserDataserverDatasource

--- test/data/datasources/user_data_server_datasource_test.dart
+++ test/data/datasources/user_data_server_datasource_test.dart
@@ -6,26 +6,30 @@ import 'package:store_client/src/data/models/user_data_model.dart';
 import 'package:store_client/src/data/models/user_model.dart';
 import 'package:store_client/src/domain/entities/role.dart';
 
-void main() {
+import '../../injector/services.dart';
+
+Future<void> main() async {
+  await initTestServices();
+
   const String currentPassword = '1234567';
   const String newPassword = '7654321';
   const String newPasswordConfirmation = '7654321';
-  const int id = 1;
+  const int id = 2;
   const String email = 'test@test.test';
+
+  final UserModel userModellogIn = UserModel(
+    id: 1,
+    userName: 'username',
+    email: email,
+  );
 
   final UserDataModel userDataModel = UserDataModel(
     id: id,
     username: "dratyti",
-    avatarUrl: "avatarUrl",
+    avatarUrl: "534",
     role: Role.user,
   );
-  final UserDataModel userDataModel1 = UserDataModel(
-    id: id,
-    username: "username",
-    avatarUrl: "avatarUrl",
-    role: Role.user,
-  );
-  final List<UserDataModel> listUserDataModel0 = [userDataModel, userDataModel1];
+  final List<UserDataModel> listUserDataModel0 = [userDataModel];
   group(
     'user_data_server_datasource_test',
     () {
@@ -35,50 +39,53 @@ void main() {
           final UserDataModel putUserDataModel = UserDataModel(
             id: id,
             username: "dratyti",
-            avatarUrl: "avatarUrl",
+            avatarUrl: "534",
             role: Role.user,
           );
-          final UserDataServerDatasource userDataServerDatasource = UserDataServerDatasource();
+          final UserDataServerDatasource userDataServerDatasource = testServices.get<UserDataServerDatasource>();
 
           final Either<Failure, UserDataModel> result = await userDataServerDatasource.putUserDataRequest(
             avatarUrl: '',
-            id: 1,
-            role: null,
+            id: 2,
+            role: Role.user,
             userName: 'dratyti',
           );
 
           final res = result.fold(
-            (Right) => userDataModel,
-            (Left) => null,
+            (left) => left,
+            (right) => right,
           );
-
+          print(result);
           expect(res, putUserDataModel);
         },
       );
       test(
         'get_all_userData_Request',
         () async {
-          final UserDataServerDatasource userDataServerDatasource = UserDataServerDatasource();
+          final UserDataServerDatasource userDataServerDatasource = testServices.get<UserDataServerDatasource>();
 
           final Either<Failure, List<UserDataModel>> userDataModel = await userDataServerDatasource.getAllUserDataRequest();
-
-          // final res = userDataModel.fold(
-          //   (Right) => listUserDataModel0,
-          //   (Left) => null,
-          // );
-          // expect(res, listUserDataModel0);
-          expect(userDataModel.isRight(), true);
+          print(userDataModel);
+          final res = userDataModel.fold(
+            (left) => left,
+            (right) => right,
+          );
+          print(res);
+          expect(res, listUserDataModel0);
         },
       );
 
       test(
         'get Concrete UserData Request',
         () async {
-          final UserDataServerDatasource userDataServerDatasource = UserDataServerDatasource();
+          final UserDataServerDatasource userDataServerDatasource = testServices.get<UserDataServerDatasource>();
 
           final Either<Failure, UserDataModel> waitResult = await userDataServerDatasource.getConcreteUserDataRequest(id: id);
 
-          final result = waitResult.fold((Right) => userDataModel, (Left) => null);
+          final result = waitResult.fold(
+            (left) => left,
+            (right) => right,
+          );
 
           expect(result, userDataModel);
         },
@@ -87,23 +94,27 @@ void main() {
       test(
         'password login method',
         () async {
-          final UserDataServerDatasource userDataServerDatasource = UserDataServerDatasource();
+          final UserDataServerDatasource userDataServerDatasource = testServices.get<UserDataServerDatasource>();
 
           final Either<Failure, UserModel> result = await userDataServerDatasource.loginUserDataRequest(
             login: email,
             password: currentPassword,
           );
 
-          expect(result.isRight(), true);
+          final res = result.fold(
+            (left) => left,
+            (right) => right,
+          );
+
+          expect(res, userModellogIn);
         },
       );
 
       test(
         'change password method',
         () async {
-          // Act.
-          final UserDataServerDatasource userDataServerDatasource = UserDataServerDatasource();
-          // Arrange.
+          final UserDataServerDatasource userDataServerDatasource = testServices.get<UserDataServerDatasource>();
+
           final Either<Failure, UserModel> result = await userDataServerDatasource.changePasswordUserDataRequest(
             currentPassword: currentPassword,
             newPassword: newPassword,
@@ -114,7 +125,7 @@ void main() {
             login: email,
             password: newPassword,
           );
-          // Accert.
+
           expect(result.isRight(), true);
           expect(user.isRight(), true);
         },
