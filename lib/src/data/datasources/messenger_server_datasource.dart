c609f3959f02830f6b2b02eb913650269a62bdfd
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-08-21 16:33:41 +0300

    fix: change failure in MessengerServerDatasource

--- lib/src/data/datasources/messenger_server_datasource.dart
+++ lib/src/data/datasources/messenger_server_datasource.dart
@@ -1,9 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:store_client/core/failure/failure.dart';
-import 'package:store_client/src/data/failure/failure.dart';
 import 'package:store_client/src/data/models/message_model.dart';
 import 'package:store_client/src/domain/entities/message.dart';
+import 'package:store_client/src/failures/trace_failures.dart';
 
 class MessengerServerDatasource {
   final Dio _client = Dio();
@@ -24,10 +24,7 @@ class MessengerServerDatasource {
       return Right(listMessage);
     } catch (e, stacktrace) {
       return Left(
-        UnhandledFailure(
-          message: e.toString(),
-          stackTrace: stacktrace,
-        ),
+        UnhandledFailure(stacktrace),
       );
     }
   }
@@ -52,10 +49,7 @@ class MessengerServerDatasource {
       return Right(unit);
     } catch (e, stacktrace) {
       return Left(
-        UnhandledFailure(
-          message: e.toString(),
-          stackTrace: stacktrace,
-        ),
+        UnhandledFailure(stacktrace),
       );
     }
   }
