24bbc0f22894f9e11c026988e474db093b0cb65a
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-08-21 18:43:00 +0300

    feat: add fetchMes, sendMes for MesServerRepositor

--- lib/src/data/repositories/remote/messenger_server_repository.dart
+++ lib/src/data/repositories/remote/messenger_server_repository.dart
@@ -1,18 +1,47 @@
 import 'package:dartz/dartz.dart';
 import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/src/data/datasources/messenger_server_datasource.dart';
 import 'package:store_client/src/domain/entities/message.dart';
 import 'package:store_client/src/domain/repository/messenger_repository.dart';
+import 'package:store_client/src/failures/trace_failures.dart';
 
 class MessengerServerRepository implements MessengerRepository {
   @override
-  Future<Either<Failure, List<Message>>> fetchMessages() {
-    // TODO: implement fetchMessages
-    throw UnimplementedError();
+  Future<Either<Failure, List<Message>>> fetchMessages() async {
+    try {
+      MessengerServerDatasource _messengerServerDatasource = services.get<MessengerServerDatasource>();
+      Either<Failure, List<Message>> result = await _messengerServerDatasource.getAllMessages();
+
+      return result.fold(
+        (failure) {
+          return Left(failure);
+        },
+        (list) {
+          return Right(list);
+        },
+      );
+    } catch (e, stacktrace) {
+      return Left(ServerFailure(stacktrace));
+    }
   }
 
   @override
-  Future<Either<Failure, Unit>> sendMessage({required Message message}) {
-    // TODO: implement sendMessage
-    throw UnimplementedError();
+  Future<Either<Failure, Unit>> sendMessage({required Message message}) async {
+    try {
+      MessengerServerDatasource _messengerServerDatasource = services.get<MessengerServerDatasource>();
+      Either<Failure, Unit> result = await _messengerServerDatasource.sendMessage(message: message);
+
+      return result.fold(
+        (failure) {
+          return Left(failure);
+        },
+        (unit) {
+          return Right(unit);
+        },
+      );
+    } catch (e, stacktrace) {
+      return Left(ServerFailure(stacktrace));
+    }
   }
 }
