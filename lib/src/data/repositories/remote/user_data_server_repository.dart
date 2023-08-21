d9c038161a5eea341772d7ded1a07fcf131c32f5
diffygg <np.01@inbox.ru>
2023-08-21 18:06:59 +0300

    feat: add checkUserConnectByJWT

--- lib/src/data/repositories/remote/user_data_server_repository.dart
+++ lib/src/data/repositories/remote/user_data_server_repository.dart
@@ -1,9 +1,15 @@
 import 'package:dartz/dartz.dart';
 import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/src/data/data_failures/data_failure.dart';
+import 'package:store_client/src/data/datasources/user_data_server_datasource.dart';
+import 'package:store_client/src/data/models/user_model.dart';
 import 'package:store_client/src/domain/entities/user_data.dart';
 import 'package:store_client/src/domain/repository/user_data_repository.dart';
 
 class UserDataServerRepository implements UserDataRepository {
+  final UserDataServerDatasource userDataServerDatasource = services.get<UserDataServerDatasource>();
+
   @override
   Future<Either<Failure, UserData>> loginUser({required String email, required String password}) {
     // TODO: implement loginUser
@@ -26,5 +32,24 @@ class UserDataServerRepository implements UserDataRepository {
   Future<Either<Failure, UserData>> changeUserName({required UserData userData, required String newUserName}) {
     // TODO: implement changeUserName
     throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, bool>> checkUserConnectByJWT() async {
+    try {
+      final Either<Failure, UserModel> userModel = await userDataServerDatasource.authMeUserDataRequest();
+      final bool authStatus = userModel.fold(
+        (l) {
+          throw l;
+        },
+        (r) {
+          return true;
+        },
+      );
+
+      return Right(authStatus);
+    } catch (e, stackTrace) {
+      return Left(DataFailure(message: e.toString(), stackTrace: stackTrace));
+    }
   }
 }
