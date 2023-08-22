6375c9ccfd0ab863614b3f68746cd0bcf9d497ad
Danissimmo <110056436+XxJ0TaR0Xx@users.noreply.github.com>
2023-08-22 22:26:30 +0300

    feat: add Failure for LibraryServerDatasource

--- lib/src/failures/trace_failures.dart
+++ lib/src/failures/trace_failures.dart
@@ -5,3 +5,15 @@ import 'package:store_client/core/failure/failure.dart';
 class UnhandledFailure extends Failure {
   UnhandledFailure(StackTrace T) : super(message: "Unhandled Failure", stackTrace: T);
 }
+
+class ServerFailureGetSections extends Failure {
+  ServerFailureGetSections(StackTrace T) : super(message: "Server error, dont found Sections", stackTrace: T);
+}
+
+class ServerFailureGetTopics extends Failure {
+  ServerFailureGetTopics(StackTrace T) : super(message: "Server error, dont found Topics", stackTrace: T);
+}
+
+class ServerFailureSendTopics extends Failure {
+  ServerFailureSendTopics(StackTrace T) : super(message: "Server error, cant send Topic", stackTrace: T);
+}
