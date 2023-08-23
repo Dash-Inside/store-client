1fceddf5dc6e8d414fb7460b67e6fbca2018b8bf
Daniil A <92711103+LazySuperintelegense@users.noreply.github.com>
2023-08-23 15:29:22 +0300

    feat: add failures

--- lib/src/data/data_failures/data_failure.dart
+++ lib/src/data/data_failures/data_failure.dart
@@ -6,3 +6,19 @@ class DataFailure extends Failure {
     required super.stackTrace,
   });
 }
+
+class UnhandledFailure extends Failure {
+  UnhandledFailure(StackTrace T) : super(message: "Unhandled Failure", stackTrace: T);
+}
+
+class ServerFailureGetSections extends Failure {
+  ServerFailureGetSections(StackTrace T) : super(message: "Server error, dont found Sections", stackTrace: T);
+}
+
+class ServerFailureGetTopics extends Failure {
+  ServerFailureGetTopics(StackTrace T) : super(message: "Server error, dont found Topics", stackTrace: T);
+}
+
+class ServerFailureSendTopics extends Failure {
+  ServerFailureSendTopics(StackTrace T) : super(message: "Server error, cant send Topic", stackTrace: T);
+}
