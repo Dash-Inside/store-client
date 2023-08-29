f570b502215c82c59be8be5573074c05bca5a1d2
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-29 15:52:59 +0300

    fix: change initServices

--- test/injector/services.dart
+++ test/injector/services.dart
@@ -1,9 +1,19 @@
 import 'dart:async';
 
 import 'package:store_client/core/services/services.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
 import 'package:store_client/src/domain/repository/messenger_repository.dart';
+import 'package:store_client/src/domain/repository/user_repository.dart';
+
+import '../domain/usecase/test_repositories.mocks.dart';
 
 FutureOr<void> initTestServices() {
-  final MessengerRepository messengerRepository = MockMessageRepository();
-  services.registerSingleton<MessengerRepository>(() => messengerRepository);
+  final MessengerRepository messengerRepository = MockMessengerRepository();
+  services.registerLazySingleton<MessengerRepository>(() => messengerRepository);
+
+  final LibraryRepository libraryRepository = MockLibraryRepository();
+  services.registerLazySingleton<LibraryRepository>(() => libraryRepository);
+
+  final UserRepository userRepository = MockUserRepository();
+  services.registerLazySingleton<UserRepository>(() => userRepository);
 }
