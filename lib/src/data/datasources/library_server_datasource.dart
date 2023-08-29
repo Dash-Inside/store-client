04da07bd623a4ea35bb1bab9712aafeda4879f56
diffygg <np.01@inbox.ru>
2023-08-29 18:05:21 +0300

    feat: add methods to LibraryServerDatasource

--- lib/src/data/datasources/library_server_datasource.dart
+++ lib/src/data/datasources/library_server_datasource.dart
@@ -1,19 +1,20 @@
+//ignore_for_file: avoid-ignoring-return-values
+
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:store_client/core/services/services.dart';
 import 'package:store_client/src/data/models/favorite_topics_model.dart';
+import 'package:store_client/src/data/models/section_model.dart';
 import 'package:store_client/src/data/models/topic_model.dart';
 import 'package:store_client/src/data/utils/status_code_handler.dart';
-import 'package:store_client/src/domain/entities/section.dart';
-import 'package:store_client/src/domain/entities/topic.dart';
 
 @Injectable()
 class LibraryServerDatasource {
   final DioModule dioModule;
-  final String _favoritUri = 'http://192.168.1.43:1337/api/favorites';
-  final String _sectiontUri = 'http://192.168.1.43:1337/api/sections';
-  final String _topictUri = 'http://192.168.1.43:1337/api/topics';
+  final String _favoritUri = 'http://127.0.0.1:1337/api/favorites';
+  final String _sectiontUri = 'http://127.0.0.1:1337/api/sections';
+  final String _topictUri = 'http://127.0.0.1:1337/api/topics';
 
   LibraryServerDatasource(this.dioModule);
 
@@ -21,7 +22,7 @@ class LibraryServerDatasource {
     try {
       FavoriteTopicsModel favoriteTopicModel = FavoriteTopicsModel(userID: 1, topicID: id);
 
-      Response response = await dioModule.client.post(
+      final Response response = await dioModule.client.post(
         _favoritUri,
         data: favoriteTopicModel.toMap(),
       );
@@ -40,50 +41,13 @@ class LibraryServerDatasource {
     required List<String> links,
   }) async {
     try {
-      return unit;
-    } catch (_) {
-      rethrow;
-    }
-  }
-
-  Future<List<Section>> getAllSectionDatasource() async {
-    try {
-      return [];
-    } catch (_) {
-      rethrow;
-    }
-  }
-
-  Future<List<Topic>> getAllTopicsBySectionIDDatasource({required int id}) async {
-    try {
-      return [];
-    } catch (_) {
-      rethrow;
-    }
-  }
-
-  Future<Topic> getTopicDataByIDDatasource({required int id}) async {
-    try {
-      final Response response = await dioModule.client.get(_topictUri + '/$id');
-
-      StatusCodeHandler.check(response.statusCode.toString());
-
-      TopicModel topicModel = TopicModel.fromMap(response.data['data']);
-
-      return Topic(
-        id: topicModel.id,
-        title: topicModel.title,
-        data: topicModel.data,
-        links: topicModel.links,
-      );
-    } catch (_) {
-      rethrow;
-    }
-  }
-
-  Future<Unit> removeFavoriteTopicDatasource({required int id}) async {
-    try {
-      final Response response = await dioModule.client.delete(_favoritUri + '/$id');
+      final Response response = await dioModule.client.post(_topictUri, data: <String, dynamic>{
+        'data': {
+          'title': title,
+          'data': data,
+          'links': links.join(';'),
+        },
+      });
 
       StatusCodeHandler.check(response.statusCode.toString());
 
@@ -93,9 +57,103 @@ class LibraryServerDatasource {
     }
   }
 
-  Future<Topic> searchTopicByTitleDatasource({required String title}) async {
+  Future<List<SectionModel>> getAllSectionDatasource() async {
     try {
-      return Topic(id: 1, title: "title", data: "data", links: ["links"]);
+      final Response response = await dioModule.client.get(_sectiontUri);
+
+      StatusCodeHandler.check(response.statusCode.toString());
+
+      final List listMap = response.data['data'];
+      final List<SectionModel> result = [];
+
+      listMap.forEach((element) {
+        result.add(SectionModel.fromMap(element as Map<String, dynamic>));
+      });
+
+      return result;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<List<int>> getAllTopicsIdBySectionIdDatasource({required int sectionId}) async {
+    try {
+      final Response response = await dioModule.client.get(_sectiontUri + '/$sectionId');
+
+      StatusCodeHandler.check(response.statusCode.toString());
+
+      final List<int> result = [];
+      result.add(response.data['data']['attributes']['topicID'] as int);
+
+      return result;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<List<TopicModel>> getAllTopicByListIdDatasource({required List<int> listTopicsId}) async {
+    try {
+      Map<String, dynamic> filters = {};
+
+      for (int i = 0; i < listTopicsId.length; i++) {
+        filters['filters[id][\$in][$i]'] = listTopicsId[i];
+      }
+
+      final Response response = await dioModule.client.get(_topictUri, queryParameters: filters);
+
+      StatusCodeHandler.check(response.statusCode.toString());
+
+      final List listMap = response.data['data'];
+      final List<TopicModel> result = [];
+
+      listMap.forEach((element) {
+        result.add(TopicModel.fromMap(element as Map<String, dynamic>));
+      });
+
+      return result;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<TopicModel> getTopicDataByIDDatasource({required int topicId}) async {
+    try {
+      final Response response = await dioModule.client.get(_topictUri + '/$topicId');
+
+      StatusCodeHandler.check(response.statusCode.toString());
+
+      return TopicModel.fromMap(response.data['data']);
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<Unit> removeFavoriteTopicDatasource({required int favoriteId}) async {
+    try {
+      final Response response = await dioModule.client.delete(_favoritUri + '/$favoriteId');
+
+      StatusCodeHandler.check(response.statusCode.toString());
+
+      return unit;
+    } catch (_) {
+      rethrow;
+    }
+  }
+
+  Future<List<TopicModel>> searchTopicByTitleDatasource({required String topicTitle}) async {
+    try {
+      final Response response = await dioModule.client.get(_topictUri, queryParameters: {'filters[title][\$contains]': topicTitle});
+
+      StatusCodeHandler.check(response.statusCode.toString());
+
+      final List listMap = response.data['data'];
+      final List<TopicModel> result = [];
+
+      listMap.forEach((element) {
+        result.add(TopicModel.fromMap(element as Map<String, dynamic>));
+      });
+
+      return result;
     } catch (_) {
       rethrow;
     }
