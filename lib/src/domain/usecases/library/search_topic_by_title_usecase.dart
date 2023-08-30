36a743d109c10df816c4897f68b47eae7df36f77
diffygg <np.01@inbox.ru>
2023-08-30 17:09:49 +0300

    feat: add validator check to SearchTopicByTitleUC

--- lib/src/domain/usecases/library/search_topic_by_title_usecase.dart
+++ lib/src/domain/usecases/library/search_topic_by_title_usecase.dart
@@ -7,17 +7,20 @@ import 'package:store_client/core/services/services.dart';
 import 'package:store_client/core/usecases/usecases.dart';
 import 'package:store_client/src/domain/entities/topic.dart';
 import 'package:store_client/src/domain/repository/library_repository.dart';
+import 'package:store_client/src/domain/validators/search_topic_validator.dart';
+import 'package:store_client/src/failures/trace_failures.dart';
 
 @Injectable()
-class SearchTopicByTitleUseCase extends UseCase<Topic, String> {
+class SearchTopicByTitleUseCase extends UseCase<List<Topic>, String> {
   final LibraryRepository libraryRepository = services.get<LibraryRepository>();
 
   @override
-  FutureOr<Either<Failure, Topic>> call(String title) {
+  FutureOr<Either<Failure, List<Topic>>> call(String title) async {
     final LibraryRepository libraryRepository = services.get<LibraryRepository>();
+    if (!(await SearchTopicValidator().validate(title))) {
+      return Left(IncorrectTopicTitleFailure(StackTrace.empty));
+    }
 
-    return libraryRepository.searchTopicByTitle(
-      title: title,
-    );
+    return libraryRepository.searchTopicByTitle(title: title);
   }
 }
