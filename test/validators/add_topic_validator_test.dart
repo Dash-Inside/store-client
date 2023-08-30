d8d7d9d984cc36da92529708e4119bb88ae59e88
diffygg <np.01@inbox.ru>
2023-08-30 18:12:51 +0300

    fix: change addTopic parameters

--- test/validators/add_topic_validator_test.dart
+++ test/validators/add_topic_validator_test.dart
@@ -1,27 +1,27 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:store_client/src/domain/usecases/library/add_topic_usecase.dart';
+import 'package:store_client/src/domain/entities/actions/add_topic_action_params.dart';
 import 'package:store_client/src/domain/validators/add_topic_validator.dart';
 
 void main() {
-  final AddTopicUseCaseParams correctAddTopicUseCaseParams = AddTopicUseCaseParams(
+  final AddTopicActionParams correctAddTopicActionParams = AddTopicActionParams(
     title: 'test',
     data: 'test data',
     links: ['http://test.xyz'],
   );
 
-  final AddTopicUseCaseParams incorrectAddTopicUseCaseParams1 = AddTopicUseCaseParams(
+  final AddTopicActionParams incorrectAddTopicActionParams1 = AddTopicActionParams(
     title: '',
     data: 'test data',
     links: ['http://test.xyz'],
   );
 
-  final AddTopicUseCaseParams incorrectAddTopicUseCaseParams2 = AddTopicUseCaseParams(
+  final AddTopicActionParams incorrectAddTopicActionParams2 = AddTopicActionParams(
     title: 'test',
     data: '',
     links: ['http://test.xyz'],
   );
 
-  final AddTopicUseCaseParams incorrectAddTopicUseCaseParams3 = AddTopicUseCaseParams(
+  final AddTopicActionParams incorrectAddTopicActionParams3 = AddTopicActionParams(
     title: 'test',
     data: 'test data',
     links: [''],
@@ -31,28 +31,28 @@ void main() {
     final AddTopicValidator addTopicValidator = AddTopicValidator();
     test('Correct Input Validator', () async {
       // Arrange
-      final result = await addTopicValidator.validate(correctAddTopicUseCaseParams);
+      final result = await addTopicValidator.validate(correctAddTopicActionParams);
       // Assert
       expect(result, true);
     });
 
     test('Incorrect Input Title Validator', () async {
       // Arrange
-      final result = await addTopicValidator.validate(incorrectAddTopicUseCaseParams1);
+      final result = await addTopicValidator.validate(incorrectAddTopicActionParams1);
       // Assert
       expect(result, false);
     });
 
     test('Incorrect Input Data Validator', () async {
       // Arrange
-      final result = await addTopicValidator.validate(incorrectAddTopicUseCaseParams2);
+      final result = await addTopicValidator.validate(incorrectAddTopicActionParams2);
       // Assert
       expect(result, false);
     });
 
     test('Incorrect Input Links Validator', () async {
       // Arrange
-      final result = await addTopicValidator.validate(incorrectAddTopicUseCaseParams3);
+      final result = await addTopicValidator.validate(incorrectAddTopicActionParams3);
       // Assert
       expect(result, false);
     });
