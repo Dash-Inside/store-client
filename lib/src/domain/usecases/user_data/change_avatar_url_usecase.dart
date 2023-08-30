96c8b1c209e629b6e5ba32f5973aa79a1d55e77a
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-30 18:30:01 +0300

    feat:  add validator for usecase

--- lib/src/domain/usecases/user_data/change_avatar_url_usecase.dart
+++ lib/src/domain/usecases/user_data/change_avatar_url_usecase.dart
@@ -6,13 +6,17 @@ import 'package:store_client/core/failure/failure.dart';
 import 'package:store_client/core/services/services.dart';
 import 'package:store_client/core/usecases/usecases.dart';
 import 'package:store_client/src/domain/entities/user.dart';
+import 'package:store_client/src/domain/failure/message_failure.dart';
+import 'package:store_client/src/domain/failure/user_data_failure.dart';
 import 'package:store_client/src/domain/repository/user_repository.dart';
+import 'package:store_client/src/domain/validators/change_avatar_url_validator.dart';
 
 @Injectable()
 class ChangeAvatarUrlUseCase extends UseCase<User, ChangeAvatarUrlUseCaseParams> {
   @override
-  FutureOr<Either<Failure, User>> call(ChangeAvatarUrlUseCaseParams params) {
+  FutureOr<Either<Failure, User>> call(ChangeAvatarUrlUseCaseParams params) async {
     final UserRepository userRepository = services.get<UserRepository>();
+    if (!(await ChangeAvatarUrlValidator().validate(params))) return Left(NotValidChangeAvatarUrlFailure());
 
     return userRepository.changeAvatarUrl(
       user: params.user,
