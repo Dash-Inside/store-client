07658a4f0f37db97c7b41bd0bebbde5386046aeb
diffygg <np.01@inbox.ru>
2023-08-30 17:15:05 +0300

    feat: add arrangeMockLibraryRepository

---
+++ test/services/arrange/mock_library_repository.dart
@@ -0,0 +1,114 @@
+// ignore_for_file: avoid-top-level-members-in-tests, prefer-static-class, avoid-dynamic
+
+import 'package:dartz/dartz.dart';
+import 'package:mockito/mockito.dart';
+import 'package:store_client/src/domain/entities/section.dart';
+import 'package:store_client/src/domain/entities/topic.dart';
+import 'package:store_client/src/failures/trace_failures.dart';
+
+import '../../mock/repositories.mocks.dart';
+
+const int correctId = 1;
+const int incorrectId = -3;
+
+final Topic correctTopic = Topic(
+  id: 1,
+  title: 'title',
+  data: 'data',
+  links: <String>['links', 'links'],
+);
+
+final Topic incorrectTopic = Topic(
+  id: 1,
+  title: '',
+  data: '',
+  links: [''],
+);
+
+MockLibraryRepository arrangeMockLibraryRepository() {
+  final mock = MockLibraryRepository();
+
+  when(
+    mock.addFavoriteTopic(id: correctId),
+  ).thenAnswer(
+    (_) async => Right(unit),
+  );
+
+  when(
+    mock.addFavoriteTopic(id: incorrectId),
+  ).thenAnswer(
+    (_) async => Left(IncorrectTopicIdFailure(StackTrace.empty)),
+  );
+
+  when(mock.addTopic(
+    title: correctTopic.title,
+    data: correctTopic.data,
+    links: correctTopic.links,
+  )).thenAnswer(
+    (realInvocation) async => Right(unit),
+  );
+
+  when(mock.addTopic(
+    title: incorrectTopic.title,
+    data: incorrectTopic.data,
+    links: incorrectTopic.links,
+  )).thenAnswer(
+    (realInvocation) async => Left(IncorrectTopicParamsFailure(StackTrace.empty)),
+  );
+
+  when(
+    mock.getAllSections(),
+  ).thenAnswer(
+    (_) async => Right([Section(id: 1, title: 'title', topicId: 1)]),
+  ); //абобус х2
+
+  when(
+    mock.getAllTopicsBySectionID(id: correctId),
+  ).thenAnswer(
+    (_) async => Right([correctTopic]),
+  ); //абобус
+
+  when(
+    mock.getAllTopicsBySectionID(id: incorrectId),
+  ).thenAnswer(
+    (_) async => Left(IncorrectSectionIdFailure(StackTrace.empty)),
+  );
+
+  when(
+    mock.getTopicDataByID(id: correctId),
+  ).thenAnswer(
+    (_) async => Right(correctTopic),
+  );
+
+  when(
+    mock.getTopicDataByID(id: incorrectId),
+  ).thenAnswer(
+    (_) async => Left(IncorrectTopicIdFailure(StackTrace.empty)),
+  );
+
+  when(
+    mock.removeFavoriteTopic(id: correctId),
+  ).thenAnswer(
+    (_) async => Right(unit),
+  );
+
+  when(
+    mock.removeFavoriteTopic(id: incorrectId),
+  ).thenAnswer(
+    (_) async => Left(IncorrectFavoriteIdFailure(StackTrace.empty)),
+  );
+
+  when(
+    mock.searchTopicByTitle(title: 'title'),
+  ).thenAnswer(
+    (_) async => Right([correctTopic]),
+  );
+
+  when(
+    mock.searchTopicByTitle(title: ''),
+  ).thenAnswer(
+    (_) async => Left(IncorrectTopicTitleFailure(StackTrace.empty)),
+  );
+
+  return mock;
+}
---
+++ test/usecases/add_favorite_topic_usecase_test.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:store_client/core/failure/failure.dart';
+import 'package:store_client/core/services/services.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+import 'package:store_client/src/domain/usecases/library/add_favorite_topic_usecase.dart';
+import 'package:store_client/src/failures/trace_failures.dart';
+
+import '../services/arrange/mock_library_repository.dart';
+import '../services/services.dart';
+
+void main() async {
+  await initMockService();
+  group('Add Favorite Topic Use Case', () {
+    test('Correct call test', () async {
+      // Act
+      final LibraryRepository libraryRepository = services<LibraryRepository>();
+      final AddFavoriteTopicUseCase addFavoriteTopicUseCase = services<AddFavoriteTopicUseCase>();
+      // Arrange
+      final Either<Failure, Unit> failOrResult = await addFavoriteTopicUseCase.call(correctId);
+      // Assert
+      expect(failOrResult.isRight(), true);
+      verify(libraryRepository.addFavoriteTopic(id: correctId)).called(1);
+      verifyNoMoreInteractions(libraryRepository);
+    });
+
+    test('Incorrect call test', () async {
+      // Act
+      final LibraryRepository libraryRepository = services<LibraryRepository>();
+      final AddFavoriteTopicUseCase addFavoriteTopicUseCase = services<AddFavoriteTopicUseCase>();
+      // Arrange
+      final Either<Failure, Unit> failOrResult = await addFavoriteTopicUseCase.call(incorrectId);
+      final bool assertResult = failOrResult.fold(
+        (Failure failure) {
+          return (failure is IncorrectTopicIdFailure);
+        },
+        (Unit unit) {
+          return false;
+        },
+      );
+      // Assert
+      expect(assertResult, true);
+      verifyNoMoreInteractions(libraryRepository);
+    });
+  });
+}
