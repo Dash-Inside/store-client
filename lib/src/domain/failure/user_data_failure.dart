2581a6be048f90b3858b21c47625227ecbecbbcc
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-30 18:29:39 +0300

    feat: create NotValidChangeAvatarUrlFailure

---
+++ lib/src/domain/failure/user_data_failure.dart
@@ -0,0 +1,5 @@
+import 'package:store_client/core/failure/failure.dart';
+
+class NotValidChangeAvatarUrlFailure extends Failure {
+  NotValidChangeAvatarUrlFailure() : super(message: 'Not Valid Change Avatar Url Failure', stackTrace: StackTrace.empty);
+}
