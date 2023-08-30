d7e488597bf444cda9a663405d4881f8f3b2040e
diffygg <np.01@inbox.ru>
2023-08-30 18:10:43 +0300

    feat: add AddTopicActionParams

---
+++ lib/src/domain/entities/actions/add_topic_action_params.dart
@@ -0,0 +1,11 @@
+class AddTopicActionParams {
+  final String title;
+  final String data;
+  final List<String> links;
+
+  AddTopicActionParams({
+    required this.title,
+    required this.data,
+    required this.links,
+  });
+}
