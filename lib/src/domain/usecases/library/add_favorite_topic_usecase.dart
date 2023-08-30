16f2418dcf3aa2f4097a4b44d0b8e4c556a271fd
diffygg <np.01@inbox.ru>
2023-08-30 16:51:01 +0300

    feat: add Validator check method

--- lib/src/domain/usecases/library/add_favorite_topic_usecase.dart
+++ lib/src/domain/usecases/library/add_favorite_topic_usecase.dart
@@ -6,15 +6,19 @@ import 'package:store_client/core/failure/failure.dart';
 import 'package:store_client/core/services/services.dart';
 import 'package:store_client/core/usecases/usecases.dart';
 import 'package:store_client/src/domain/repository/library_repository.dart';
+import 'package:store_client/src/domain/validators/add_favorite_topic_validator.dart';
+import 'package:store_client/src/failures/trace_failures.dart';
 
 @Injectable()
 class AddFavoriteTopicUseCase extends UseCase<Unit, int> {
   @override
-  FutureOr<Either<Failure, Unit>> call(int topicId) {
+  FutureOr<Either<Failure, Unit>> call(int topicId) async {
     final LibraryRepository libraryRepository = services.get<LibraryRepository>();
 
-    return libraryRepository.addFavoriteTopic(
-      id: topicId,
-    );
+    if (!(await AddFavoriteTopicValidator().validate(topicId))) {
+      return Left(IncorrectTopicIdFailure(StackTrace.empty));
+    }
+
+    return libraryRepository.addFavoriteTopic(id: topicId);
   }
 }
