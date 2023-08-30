f1c2cf3ca91f64de937a205164ed185f9c124701
diffygg <np.01@inbox.ru>
2023-08-30 17:10:13 +0300

    feat: add AddFavoriteTopicValidator

---
+++ lib/src/domain/validators/add_favorite_topic_validator.dart
@@ -0,0 +1,10 @@
+import 'dart:async';
+
+import 'package:store_client/core/validator/validator.dart';
+
+class AddFavoriteTopicValidator extends Validator<int> {
+  @override
+  FutureOr<bool> validate(int t) {
+    return !t.isNegative;
+  }
+}
