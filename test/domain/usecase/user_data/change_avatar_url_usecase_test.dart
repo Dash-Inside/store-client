f78ba4b9eda72209c25d606b72a1dd1b8f3d94e4
dexorded <126596523+dexorded@users.noreply.github.com>
2023-08-30 17:36:50 +0300

    feat: init test for usecase UserDataRepository

--- test/domain/usecase/user_data/change_avatar_url_usecase_test.dart
+++ test/domain/usecase/user_data/change_avatar_url_usecase_test.dart
@@ -4,52 +4,64 @@ import 'package:mockito/mockito.dart';
 import 'package:store_client/core/failure/failure.dart';
 import 'package:store_client/core/services/services.dart';
 import 'package:store_client/src/domain/entities/message.dart';
+import 'package:store_client/src/domain/entities/user.dart';
 import 'package:store_client/src/domain/repository/messenger_repository.dart';
+import 'package:store_client/src/domain/repository/user_repository.dart';
 import 'package:store_client/src/domain/usecases/messenger/send_message_usecase.dart';
+import 'package:store_client/src/domain/usecases/user_data/change_avatar_url_usecase.dart';
 
 import '../../../injector/services.dart';
 
+const String newAvatarUrl =
+    'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.canva.com%2Fru_ru%2Fobuchenie%2Fidei-dlya-foto%2F&psig=AOvVaw27kpS7isvaEMvZZ3m2S_oR&ust=1693492310569000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCLClvM_MhIEDFQAAAAAdAAAAABAE';
+
+const String incorrectAvatarUrl = '';
+final User user = User(
+  id: user.id,
+  email: user.email,
+  username: user.username,
+  avatarUrl: user.avatarUrl,
+  role: user.role,
+);
+
+final ChangeAvatarUrlUseCaseParams changeAvatarUrlUseCaseParams = ChangeAvatarUrlUseCaseParams(
+  user,
+  newAvatarUrl,
+);
+
+final ChangeAvatarUrlUseCaseParams incorrectChangeAvatarUrlUseCaseParams = ChangeAvatarUrlUseCaseParams(
+  user,
+  incorrectAvatarUrl,
+);
 void main() async {
-  final Message correctMessage = Message(
-    id: 1,
-    data: "data",
-    senderId: 3,
-  );
-
-  final Message incorrectMessage = Message(
-    id: -1,
-    data: "data",
-    senderId: 3,
-  );
-
   await initMockServices();
   await initUseCaseServices();
   group('send message usecase test', () {
     test('correct send message test', () async {
       //Act.
-      final MessengerRepository messengerRepository = services<MessengerRepository>();
-      final SendMessageUsecase sendMessageUsecase = services<SendMessageUsecase>();
+      final UserRepository userRepository = services<UserRepository>();
+      final ChangeAvatarUrlUseCase changeAvatarUrlUseCase = services<ChangeAvatarUrlUseCase>();
 
       //Arrange.
-      final Either<Failure, Unit> failOrMessage = await sendMessageUsecase.call(correctMessage);
+      final Either<Failure, User> failOrAvatar = await changeAvatarUrlUseCase.call(changeAvatarUrlUseCaseParams);
 
       //Accert.
-      expect(failOrMessage.isRight(), true);
-      verify(messengerRepository.sendMessage(message: correctMessage)).called(1);
-      verifyNoMoreInteractions(messengerRepository);
+      expect(failOrAvatar.isRight(), true);
+      verify(userRepository.changeAvatarUrl(user: user, newAvatarUrl: newAvatarUrl)).called(1);
+      verifyNoMoreInteractions(userRepository);
     });
 
     test('incorrect send message test', () async {
       //Act.
-      final MessengerRepository messengerRepository = services<MessengerRepository>();
-      final SendMessageUsecase sendMessageUsecase = services<SendMessageUsecase>();
+      final UserRepository userRepository = services<UserRepository>();
+      final ChangeAvatarUrlUseCase changeAvatarUrlUseCase = services<ChangeAvatarUrlUseCase>();
 
       //Arrange.
-      final Either<Failure, Unit> failOrMessage = await sendMessageUsecase.call(incorrectMessage);
+      final Either<Failure, User> failOrAvatar = await changeAvatarUrlUseCase.call(incorrectChangeAvatarUrlUseCaseParams);
 
       //Accert.
-      expect(failOrMessage.isLeft(), true);
-      verifyNoMoreInteractions(messengerRepository);
+      expect(failOrAvatar.isLeft(), true);
+      verifyZeroInteractions(userRepository);
     });
   });
 }
