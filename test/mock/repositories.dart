1757918f90f46a4635fef2ef681edecff85b2a31
diffygg <np.01@inbox.ru>
2023-08-30 17:13:21 +0300

    feat: add generator for MockLibraryRepository

---
+++ test/mock/repositories.dart
@@ -0,0 +1,5 @@
+import 'package:mockito/annotations.dart';
+import 'package:store_client/src/domain/repository/library_repository.dart';
+
+@GenerateNiceMocks([MockSpec<LibraryRepository>()])
+import 'repositories.mocks.dart';
